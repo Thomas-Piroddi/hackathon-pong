34e494859dda16514942de05dfe0e70727c36ac0
MeikaFreckelton <meikafreckelton@gmail.com>
2020-10-20 18:54:29 +1100

    added styling

--- app.js
+++ app.js
@@ -1,29 +1,54 @@
 const express = require('express')
 const app = express()
-
+const port = 3000
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
 
+const pageRouter = require('./routes/page_routes')
+// import { socker } from './socker'
+// import { API_PORT, host } from './env'
+// import http from 'http'
+// const server = new http.Server(app)
+// socker(server)
+
+// app.lsiten(API_PORT, () => {
+//     logger.info(`Api listening on port ${Number(API_PORT)} `)
+// })
+
+// server.listen(Number(API_PORT) + 1, () => {
+//     logger.info(`Socker listening on port ${Number(API_PORT) + 1}!`)
+//     logger.info(`Api and socker whitelisted for ${host}`)
+
+// })
+
 // socket io attempt --------------------
-// const http = require('http')
-// const path = require('path')
+const http = require('http')
+const path = require('path')
 
 
-// const socketio = require('socket.io')
-// const server = http.createServer(app)
-// const io = socketio(server)
+const socketio = require('socket.io')
+const server = http.createServer(app)
+const io = socketio(server)
+
+// server.listen(4000, () => {
+//     console.log('listening on *:4000');
+// });
 
 // app.use(express.static(path.join(__dirname, "public")))
 // --------------------------------
 
+// io.on('connection', (socket) => {
+//     console.log('a user connected:', socket.id);
+//     socket.on('disconnect', function() {
+//       console.log('user disconnected');
+//     });
+// });
 
-const port = 3000
+
 
 
 app.use(express.static('public'))
-
-const pageRouter = require('./routes/page_routes')
 
 
 // connect to local database
@@ -55,32 +80,36 @@ app.listen(port, () => {
 
 
 // for socket tutorial :
-// server.listen(port, () => {
-//     console.log(`Blog express app listening on port ${port}`);
-// });
+server.listen(4000, () => {
+    console.log(`server listening on port ${4000}`);
+});
 
-// const connections = [null, null]
+const connections = [null, null]
 
-// io.on('connection', socket => {
-//     // console.log("new WS connection")
+io.on('connection', socket => {
+    console.log("new WS connection")
 
-//     // find available player number
-//     let playerIndex = -1
-//     for (const i in connections){
-//         if(connections[i] === null){
-//             playerIndex = i
-//             break
-//         }
-//     }
-
+    // find available player number
+    let playerIndex = -1
+    for (const i in connections){
+        if(connections[i] === null){
+            playerIndex = i
+            break
+        }
+    }
    
 
-//     // tell the connecting client what player number they are
-//     socket.emit('player-number', playerIndex)
+    // tell the connecting client what player number they are
+    socket.emit('player-number', playerIndex)
 
-//     console.log(`Player ${playerIndex} has connected`)
+    console.log(`Player ${playerIndex} has connected`)
 
-//     // ignore player 3
-//     if (playerIndex === -1) return
+    // ignore player 3
+    if (playerIndex === -1) return
 
-// })
+    connections[playerIndex] = false
+
+    // tell everyone what player num just connected
+    socket.broadcast.emit('player-connection', playerIndex)
+
+})
